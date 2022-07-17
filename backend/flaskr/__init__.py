5f868790354529b076be001386d3615337e53d03
Aminu Kano <aminukano585@gmail.com>
2022-07-17 03:47:07 +0100

    handle server error

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -311,6 +311,16 @@ def create_app(test_config=None):
             }
         ), 422
 
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify(
+            {
+                'success': False,
+                'error': 500,
+                'message': 'Server error'
+            }
+        ), 500
+
 
     return app
 
