f052051513ffc89657f1ede9abaf22239238868b
Aminu Kano <aminukano585@gmail.com>
2022-07-17 03:16:52 +0100

    Add secrets to env

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,10 +1,19 @@
 import os
+from dotenv import load_dotenv
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
+
 from flaskr import create_app
 from models import setup_db, Question, Category
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+TEST_DB_NAME = os.getenv('TEST_DB_NAME', 'trivia_test')
+TEST_DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, TEST_DB_NAME)
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +23,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = TEST_DB_PATH
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
